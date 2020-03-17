33964e3cc443922c8cb6c43fb937371a3242f042
chandra20500 <50678178+chandra20500@users.noreply.github.com>
2020-03-17 18:11:44 +0530

    empty

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './stylesheets/index.css';
-import App from './App';
-import './stylesheets/index.css'
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
